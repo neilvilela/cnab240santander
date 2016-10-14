fd5e19eef674abac9e28ff098cf3172f4aa85d01
Neil Carvalho <me@neil.pro>
2016-10-14 11:10:11 -0300

    Altera os specs para seguirem algumas boas práticas do RSpec

--- spec/files/cnab_spec.rb
+++ spec/files/cnab_spec.rb
@@ -1,31 +1,29 @@
 require "spec_helper"
 
 describe "Cnab240santander" do
-  it "should include ActiveSupport" do
-    Object.class_eval { const_defined?("ActiveSupport") }.should be_true
+  it "includes ActiveSupport" do
+    Object.class_eval { const_defined?("ActiveSupport") }.should be_truthy
   end
   
-  it "should raise an exception when :path options is nil" do
-    lambda {
-      Cnab240santander.retorno()
-    }.should raise_exception(Exception)
+  it "raises an exception when :path options is nil" do
+    expect { Cnab240santander.retorno }.to raise_error(ArgumentError)
   end
   
-  it "should return an array" do
-    cnab = Cnab240santander.retorno(:path => File.dirname(__FILE__)+"/../../RETORNO.TXT", :retorna => nil)
+  it "returns an array" do
+    cnab = Cnab240santander.retorno(path: File.dirname(__FILE__)+"/../../RETORNO.TXT", retorna: nil)
     cnab.linhas.should be_kind_of(Array)
   end
 
   context "Header file" do
-    it "should parse the header file" do
-      cnab = Cnab240santander.retorno(:path => File.dirname(__FILE__)+"/../../RETORNO.TXT", :retorna => 0)
+    it "parses the header file" do
+      cnab = Cnab240santander.retorno(path: File.dirname(__FILE__)+"/../../RETORNO.TXT", retorna: 0)
       cnab.linhas.should be_kind_of(Array)
     end
   end
 
   context "Details merged" do
-    it "should parse the details and merge the U and T segments" do
-      cnab = Cnab240santander.retorno(:path => File.dirname(__FILE__)+"/../../RETORNO.TXT", :retorna => 3, :merge => true)
+    it "parses the details and merge the U and T segments" do
+      cnab = Cnab240santander.retorno(path: File.dirname(__FILE__)+"/../../RETORNO.TXT", retorna: 3, merge: true)
       cnab.linhas.should be_kind_of(Array)
     end
   end

5ea7839df251bc07f509bb69e0a33038d71ebd07
Neil Carvalho <me@neil.pro>
2016-10-14 11:49:16 -0300

    Melhora os testes, testando valores específicos

--- spec/files/cnab_spec.rb
+++ spec/files/cnab_spec.rb
@@ -11,20 +11,31 @@ describe "Cnab240santander" do
   
   it "returns an array" do
     cnab = Cnab240santander.retorno(path: File.dirname(__FILE__)+"/../../RETORNO.TXT", retorna: nil)
-    cnab.linhas.should be_kind_of(Array)
+    expect(cnab.linhas).to be_kind_of(Array)
   end
 
-  context "Header file" do
+  context "Header" do
     it "parses the header file" do
       cnab = Cnab240santander.retorno(path: File.dirname(__FILE__)+"/../../RETORNO.TXT", retorna: 0)
-      cnab.linhas.should be_kind_of(Array)
+      header = cnab.linhas.first
+
+      expect(cnab.linhas).to be_kind_of(Array)
+      expect(header['nome_banco'].strip).to eq 'BANCO SANTANDER (BRASIL) S/A'
+      expect(header['data_geracao_arq'].strip).to eq '04082011'
     end
   end
 
   context "Details merged" do
     it "parses the details and merge the U and T segments" do
       cnab = Cnab240santander.retorno(path: File.dirname(__FILE__)+"/../../RETORNO.TXT", retorna: 3, merge: true)
-      cnab.linhas.should be_kind_of(Array)
+      linha = cnab.linhas.first
+
+      expect(cnab.linhas).to be_kind_of(Array)
+      puts linha
+      expect(linha['nosso_numero']).to eq '0000000000060'
+      expect(linha['valor_tarifa']).to eq 2.64
+      expect(linha['data_credito']).to eq '05/08/2011'
+      expect(linha['rejeicoes']).to eq '0400000000'
     end
   end
 end
